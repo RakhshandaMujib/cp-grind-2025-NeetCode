43bd729d41678543ff335413ec9e3242c89eaff6
RakhshandaMujib <47360491+RakhshandaMujib@users.noreply.github.com>
2025-07-31 21:39:04 +0530

    Modified the solution to 3Sum problem with better comments
    --------------
    Brief comment:
    --------------
    
    - Sorted the array to simplify duplicate avoidance and enable two-pointer strategy
    - Used early exit when current number > 0 (no valid triplets possible)
    - Skipped duplicate first numbers to prevent duplicate triplets
    - Added in-loop duplicate skipping for left pointer after finding a valid triplet
    - Time complexity: O(n^2), Space: O(1) (excluding output list)

--- Neetcode-150/02 Two pointers/03 3Sum.py
+++ Neetcode-150/02 Two pointers/03 3Sum.py
@@ -8,37 +8,50 @@ class Solution:
         result = []
         nums.sort()
 
+
         for i in range(len(nums)):
             curr_num = nums[i]
 
-            if curr_num > 0:  # Can't make zero-sum beyond this
-                break
-            if i > 0 and curr_num == nums[i - 1]:
-                continue  # Skip duplicate first numbers
+            # If the current number > 0, there is no way
+            # we can form a 0 by adding anything up to it
+            # (but we can have >= 3 0s in which case [0 0 0]
+            # will be a valid triplet, so not using >= 0)
+            if curr_num > 0:
+                return result
 
-            low_p, high_p = i + 1, len(nums) - 1
+            target_sum = 0 - curr_num
+            
+            # We are not looking at the first number
+            # and, the previous number is not the same
+            # as the current number to avoid duplicate triplets
+            if i > 0 and nums[i - 1] == curr_num: 
+                continue
+            
+            left_p, right_p = i + 1, len(nums) - 1
+            
+            while left_p < right_p:
+                curr_sum = nums[left_p] + nums[right_p]
 
-            while low_p < high_p:
+                # Move inwards
+                if curr_sum < target_sum:
+                    left_p += 1
+                elif curr_sum > target_sum:
+                    right_p -= 1
 
-                three_sum = curr_num + nums[low_p] + nums[high_p]
-
-                if three_sum < 0:
-                    low_p += 1
-
-                elif three_sum > 0:
-                    high_p -= 1
-
+                # Current sum == target sum
                 else:
-                    result.append([curr_num, nums[low_p], nums[high_p]])
+                    # Successful triplet found
+                    result.append([curr_num, nums[left_p], nums[right_p]])
                     
-                    # Skip duplicates for both pointers
-                    while low_p < high_p and nums[low_p] == nums[low_p + 1]:
-                        low_p += 1
-                    while low_p < high_p and nums[high_p] == nums[high_p - 1]:
-                        high_p -= 1
-                    
-                    # Move inward
-                    low_p += 1
-                    high_p -= 1
-                    
+                    # Move to look for other 2-sum pair that will make a
+                    # valid triplet for the current number
+                    left_p += 1
+
+                    # Handle duplicate pairs in the 2-sum:
+                     # Skip the duplicate left numbers (or right numbers, any one)
+                     # No need of handling the right number duplicates because
+                     # a + b will never be equal to c + b if a != c
+=                    while nums[left_p] == nums[left_p - 1] and left_p < right_p:
+                        left_p += 1
+
         return result
